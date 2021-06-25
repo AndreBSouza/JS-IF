e9611b78297fcfb6c559fffcfb8b8448a4ae0ff1
André Bastilhos <50469267+andreeeeeeeee@users.noreply.github.com>
2021-06-25 17:47:56 -0300

    Update and rename Lista 05.07 to Lista 05-07.js

--- 2° Bim./Lista 05-07.js
+++ 2° Bim./Lista 05-07.js
@@ -1,9 +1,9 @@
 //1. mostrar os n primeiros números primos
-var n=parseInt(prompt("Digite o número de primos que tu quer:"));
-var qn=0
-for (var d=0; d<N; d++) {
-    if (N%d==0) {
-       qn++
+var n = parseInt(prompt("Digite o número de primos que tu quer:"));
+var qn = 0
+for (var d = 0; d < N; d++) {
+    if (N % d == 0) {
+        qn++
     }
 }
 //RESOLUÇÃO: PEGA OS DIVISORES DOS NÚMEROS E COLOCA UM CONTADOR NOS QUE TEM SÓ DOIS AAAAAAAAAAAAAAAAAA
@@ -12,18 +12,18 @@ for (var d=0; d<N; d++) {
 
 
 //2. os divisores de 12 são 1, 2, 3, 4, 6 e 12. o maior divisor de 12 que não excede a raiz quadrada de 12 é 3. podemos chamar o maior divisor de um inteiro n que não excede a raiz quadrada de n de pseudo raiz quadrada(PRQ). faça um programa que calcule PRQ sem usar funções matemáticas.
-var n=parseInt(prompt("Digite um número: "));
-for (var d=0; d<N; d++) {
-    if (N%d==0) {
-       qn++
+var n = parseInt(prompt("Digite um número: "));
+for (var d = 0; d < N; d++) {
+    if (N % d == 0) {
+        qn++
     }
 }
 
 //3. calcular potenciação sem funções matemáticas
-var N1=parseInt(prompt("Digite a base:"));
-var N2=parseInt(prompt("Digite o expoente:"));
-var a=N1;
-for(var x=1;x<N2;x++) {
-    a*=N1;
+var N1 = parseInt(prompt("Digite a base:"));
+var N2 = parseInt(prompt("Digite o expoente:"));
+var a = N1;
+for (var x = 1; x < N2; x++) {
+    a *= N1;
 }
-alert(N1+" ^ "+N2+" = "+a);
+alert(N1 + " ^ " + N2 + " = " + a);
